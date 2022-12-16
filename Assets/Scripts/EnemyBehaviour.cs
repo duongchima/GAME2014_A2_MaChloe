cf15e95f835f7c994fe9e0eca604f3bcd81d1d2b
Duong-Chi Ma <ma.duongchi@gmail.com>
2022-12-16 14:17:51 -0500

    Created WaveSpawner script

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 
 public class EnemyBehaviour : MonoBehaviour
 {
-    PlayerBehaviour player;
+
 
     private void Awake()
     {
-        player = FindObjectOfType<PlayerBehaviour>();
     }
 
     // Start is called before the first frame update
@@ -20,14 +19,6 @@ public class EnemyBehaviour : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector2 pos = transform.position;
 
-        pos.x = player.velocity.x * Time.fixedDeltaTime;
-        if(pos.x < -100)
-        {
-            Destroy(gameObject);
-        }
-
-        transform.position = pos;
     }
 }
